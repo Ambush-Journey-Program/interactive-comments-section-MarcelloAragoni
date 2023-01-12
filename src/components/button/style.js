049d48bc5bf767b083abf50adfaedf7ff6ce5a32
Marcello <marcello_gp_@hotmail.com>
2023-01-12 14:45:23 -0300

    Created button variants

--- src/components/button/style.js
+++ src/components/button/style.js
@@ -1,13 +1,40 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const SendButton = styled.button`
+const variants = {
+  send: css`
+    color: #fff;
+    background-color: #5357b6;
+    text-transform: uppercase;
+
+    & svg {
+      display: none;
+    }
+  `,
+  ghost: css`
+    color: #5357b6;
+    background-color: transparent;
+    text-transform: none;
+  `,
+  delete: css`
+    color: #ed6368;
+    background-color: transparent;
+    text-transform: none;
+  `
+};
+
+export const Button = styled.button`
+  position: relative;
   height: 3rem;
   width: 6.5rem;
   padding: 0 1.33px;
   font-weight: bold;
-  color: white;
-  background-color: #5357b6;
   cursor: pointer;
   border: none;
   border-radius: 0.3rem;
+
+  & svg {
+    margin-right: 10px;
+  }
+
+  ${({ variant }) => variants[variant]}
 `;
